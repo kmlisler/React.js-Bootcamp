b433d753673edf992215e7775c5f48582b50ec0c
kmlisler <82888052+kmlisler@users.noreply.github.com>
2022-04-17 02:09:07 +0300

    finished

---
+++ Week 4 - Random Image Gallery/script.js
@@ -0,0 +1,9 @@
+
+
+
+for (let i = 0; i < 15; i++) { // there will be 15 img because this is what we are asked for
+    imagesRow = document.getElementsByClassName("imagesRow");
+    // we added a different parameter with "?i". All images have different i and we get different random multiple images.
+    imagesRow[0].innerHTML += '<div class="col-md-4"><img src="https://source.unsplash.com/random/370x370?'+i+'"  class="img-fluid img-rounded mb-4"  /></div>';
+    console.log('aaa')
+}
